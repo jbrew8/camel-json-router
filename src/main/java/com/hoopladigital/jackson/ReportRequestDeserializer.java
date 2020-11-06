768d2a6039039b82b901946f72339b5c8fdb3cfc
Larry Meadors <larry.meadors@dillonsoftware.com>
2020-11-06 08:44:47 -0700

    fix: deal with nulls better
    fix: simplify message routing

--- src/main/java/com/hoopladigital/jackson/ReportRequestDeserializer.java
+++ src/main/java/com/hoopladigital/jackson/ReportRequestDeserializer.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.hoopladigital.bean.ReportRequest;
 import com.hoopladigital.bean.ReportType;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
 import static com.fasterxml.jackson.databind.type.SimpleType.constructUnsafe;
 
+@Slf4j
 public class ReportRequestDeserializer extends StdDeserializer<ReportRequest<?>> {
 
 	public ReportRequestDeserializer(final Class<?> vc) {
@@ -27,17 +29,34 @@ public class ReportRequestDeserializer extends StdDeserializer<ReportRequest<?>>
 		final JsonNode node = jsonParser.getCodec().readTree(jsonParser);
 
 		// extract the report type and create our request
-		final ReportType reportType = ReportType.valueOf(node.get("reportType").asText());
+		final JsonNode jsonNode = node.get("reportType");
+		final ReportType reportType;
+		if (jsonNode != null) {
+			reportType = ReportType.valueOf(jsonNode.asText());
+			log.debug("using report type {}", reportType);
+		} else {
+			log.warn("no report type found - deserializing will not be correct for this json");
+			reportType = null;
+		}
 		final ReportRequest<?> request = new ReportRequest<>(reportType);
 
-		// now we can get the payload...
-		final JsonParser payloadParser = node.get("payload").traverse();
+		if (null != reportType) {
 
-		// deserializing payload fails with "unexpected end of input" if we don't call nextToken() first
-		payloadParser.nextToken();
+			// since we know the type, we can get and parse the payload...
+			final JsonNode payload = node.get("payload");
+			if (null != payload) {
 
-		// ...and deserialize it
-		request.setPayload(context.readValue(payloadParser, constructUnsafe(reportType.getPayloadClass())));
+				final JsonParser payloadParser = payload.traverse();
+
+				// deserializing payload fails with "unexpected end of input" if we don't call nextToken() first
+				payloadParser.nextToken();
+
+				// ...and deserialize it
+				request.setPayload(context.readValue(payloadParser, constructUnsafe(reportType.getPayloadClass())));
+
+			}
+
+		}
 
 		return request;
 
